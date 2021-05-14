629724d7ffd1721b64c666309910f243cf06be6a
CeliaTrache <celiatrache@gmail.com>
2021-05-14 12:09:28 +0200

    add image upload

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,12 +14,12 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     @list.save
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
